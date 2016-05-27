0371b4c779fb8dee305780df9776254b8d02b3f9
dokeun_oh <ohdoking@softwareinlfie.com>
2016-05-27 15:12:50 +0900

    jquery 플러그인 추가로 popup 제작

---
+++ public/js/popup.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+
+  $('#appear').on("click",function(){
+    $('#popupDialog').popup({
+      title: "title",
+      content: "content text",
+      footer: {
+        ok: function(e,self) {
+          e.preventDefault();
+          console.log("ok");
+
+          
+          
+        },
+        cancel: function(e,self) {
+          e.preventDefault();
+          console.log("cancel");
+        }
+      }
+    });
+  });
+  
+});
+
+$.fn.popup = function(options) {
+  var defaults = {
+    width: 400,
+    height: 400,
+  };
+
+  var options = $.extend(defaults, options);
+
+  this.each(function() {
+
+    $this = $(this);
+
+    var elem = {
+      "$container" : $('body'),
+      "$dialogBackground" : $('<div class="ui-popup-screen ui-overlay-block in" id="popupDialog-screen"></div>'),
+      "$dialog" : $('<div id="popupDialog" class="ui-popup-container ui-overlay-shadow ui-corner-all"></div>'),
+      "$dialogBody" : $('<div role="popup" id="popupDialog" class="ui-popup ui-body-b ui-corner-all">'),
+      "$dialogHeader" : $('<div role="heading" class="ui-header"><h1 class="ui-title" id="popup-header"></h1></div>'),
+      "$dialogContent" : $('<div role="main" class="ui-content"><p id="popup-content"></p></div>'),
+      "$dialogFooter" : $('<div role="footer" class="ui-footer"></div>'),
+      "$dialogOkButton" : $('<a href="#" class="ui-btn ui-corner-all">OK</a>'),
+      "$dialogCancelButton" : $('<a href="#" class="ui-btn ui-corner-all ui-btn-cancel">Cancel</a>'),
+    };
+
+    $this.before(elem.$dialogBackground);
+    $this.addClass("ui-popup-container");
+    $this.addClass("pop");
+    $this.addClass("in");
+    $this.addClass("ui-popup-active");
+
+    //css 설정    
+    $this.css("width", options.width);
+    $this.css("height", options.height);
+    elem.$dialogBody.css("width", options.width);
+    elem.$dialogBody.css("height", options.height);
+    elem.$dialogFooter.css("width", options.width);
+
+    //가운대 정렬
+    $this.css("left", (elem.$container.width()-$this.width())/2);
+    $this.css("top", (elem.$container.height()-$this.height())/2)
+    $this.append(elem.$dialogBody);
+
+   
+
+    /*text 입력*/
+    elem.$dialogHeader.text(options.title);
+    elem.$dialogContent.text(options.content);
+
+
+    /* button event 등록 */
+    elem.$dialogOkButton.on("click", function(){
+      elem.$dialogBackground.remove();
+      $this.empty();
+      $this.removeAttr('class style');
+      if (options.footer.ok !== null && typeof options.footer.ok == "function") {
+        result = options.footer.ok(event, $this);
+      }
+      return result;
+    });
+
+    elem.$dialogCancelButton.on("click", function(){
+      elem.$dialogBackground.remove();
+      $this.empty();
+      $this.removeAttr('class style');
+      if (options.footer.cancel !== null && typeof options.footer.cancel == "function") {
+        result = options.footer.cancel(event, $this);
+      }
+      return result;
+    });
+
+
+    /* elemnet 구성*/
+    elem.$dialogFooter.append(elem.$dialogOkButton);
+    elem.$dialogFooter.append(elem.$dialogCancelButton);
+    elem.$dialogBody.append(elem.$dialogHeader);
+    elem.$dialogBody.append(elem.$dialogContent);
+    elem.$dialogBody.append(elem.$dialogFooter);
+
+   
+  });
+
+  return this;
+}
